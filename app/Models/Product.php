f580e98c98b63640a10ccca8c77c44664e0819d8
curder <curder@foxmail.com>
2017-12-02 19:27:35 +0800

    定义数据模型关联关系

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,7 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property mixed $reviews
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property int $id
+ */
 class Product extends Model
 {
-    //
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -4,7 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property \Carbon\Carbon $created_at
+ * @property int $id
+ * @property \Carbon\Carbon $updated_at
+ * @property mixed $product
+ */
 class Review extends Model
 {
-    //
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
