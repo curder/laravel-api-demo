501d63b6cd332e01600dba053d3eb0218afdef06
curder <curder@foxmail.com>
2017-12-02 18:53:42 +0800

    创建API相关模型、控制器和路由等

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::ApiResource('/products', 'ProductController');
+Route::group(['prefix'=>'products'],function(){
+    Route::apiResource('/{product}/reviews','ReviewController');
+});
